10a826803ad2f361f53cc874be00ffd13d7522b2
Samuel Beck <88195766+VENTIOR@users.noreply.github.com>
2022-07-05 00:37:11 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,11 @@
-DISCORD_TOKEN = "NzI2NTQ5ODQyNjk1ODE1MjMw.Xve6RQ.1eDTfkYNeVye3YU6MlBZ7CViWwI"
+DISCORD_TOKEN = ""
 COMMAND_PREFIX = "-"
 ACTIVITY1 = "-help"
 ACTIVITY2 = "Use -help for help"
-REDDIT_APP_ID="Zd09RzeD5RYSCw"
-REDDIT_APP_SECRET= "LU3JXTyFdbxY7HOrsbmSsYPbY-Y"
-REDDIT_SUBS= ['dankmemes','memes','ich_iel']
+
+# For the use of the PRAW Library, if you don't wanna use it, delete the extension from MainBot.py
+REDDIT_APP_ID= ""
+REDDIT_APP_SECRET= ""
+REDDIT_SUBS= ['']
 REDDIT_NSFW_SUBS = ['']
-user_agent =  "Benni:%s:1.0 by u/Venti0r"
+user_agent = ""
